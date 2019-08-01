7d0a52f172e979a7da838db42bb903a264b301e7
KhanShaheb34 <shakirul34@student.sust.edu>
2019-08-02 02:18:25 +0600

    Models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    thumbnail = models.ImageField(upload_to='')
+    title = models.TextField(max_length=150)
+    content = models.TextField()
+    dateCreated = models.DateField()
--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    image = models.ImageField(upload_to='')
+    title = models.TextField(max_length=150)
+    summary = models.TextField(max_length=300)
+    started = models.DateField()
+    ended = models.DateField()
+    assignedTo = models.TextField(max_length=150)
