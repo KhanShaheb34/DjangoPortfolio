098d268251a3580288368966569d2dbb8862d2f6
KhanShaheb34 <shakirul34@student.sust.edu>
2019-08-04 02:51:16 +0600

    Fetching data from psql db

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def home(req):
+    projects = Project.objects
+    return render(req, 'home/home.html', { 'projects':projects })
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+import home.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home.views.home)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
